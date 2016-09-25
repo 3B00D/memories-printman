6ec03feca745da68a308370f9d9d787be12706c0
yassermog <yassermoghrabiah@gmail.com>
2016-09-25 17:24:23 +0800

    add client Id to the order

--- bot/orders.js
+++ bot/orders.js
@@ -11,30 +11,32 @@ TABLENAME = "Orders";
 
 module.exports = {
 
-	createOrder :function (){
+    createOrder :function (userId,callback){
         id=shortid.generate()+"";
-        console.log(id);
+        //console.log(id);
         var data1={"TableName": TABLENAME,
             Item: {
                 "Id": id,
-                "firstName": " ",
-                "lastName":" ",
-                "address": " ",
-                "phoneNumber": " ",
-                "theme": " ",
-                "themetype": " ",
-                "paperType": " ",
+                "userId": userId,
+                "firstName": "#",
+                "lastName":"#",
+                "address": "#",
+                "phoneNumber": "#",
+                "theme": "#",
+                "themetype": "#",
+                "paperType": "#",
                 "completed": 0,
-                "postcode": " "
+                "postcode": "#"
             }
         };
 
-		var result=createRow(data1,function(err,data)
+        var result=createRow(data1,function(err,data)
             {
-                console.log(data);
+                //console.log(data);
+                callback(err,data1.Item);
             }
         );
-	},
+    },
     getItem :function (id,callback){            
             readRow(id, function(err,data) {
                  callback(err,data);
@@ -47,7 +49,7 @@ module.exports = {
               if (data.hasOwnProperty(key)) {
                 for (var key2 in data[key]) {
                     //console.log(key2 + " -> " + data[key][key2]);
-                    if(data[key][key2]==" ")
+                    if(data[key][key2]=="#")
                     {
                         emptyAttr.push(key2);
                     }
@@ -59,11 +61,37 @@ module.exports = {
         });
         
     },
+    getOrderStatus :function (id,callback){
+        readRow(id, function(err,data) {
+            emptyAttr=[];
+            for (var key in data) {
+              if (data.hasOwnProperty(key)) {
+                for (var key2 in data[key]) {
+                    //console.log(key2 + " -> " + data[key][key2]);
+                    if(data[key][key2]=="#")
+                    {
+                        emptyAttr.push(key2);
+                    }
+                }
+                
+              }
+            }
+            callback(err,emptyAttr);
+        });
+        
+    },
+
+    CheckClientOrders :function (clientId,callback){
+        readSecandory(clientId,"userId-index-2","userId",function(err,data) {           
+            callback(err,data);
+        });    
+    },
+
 };
     
     function createRow(data1,callback){
        
-        console.log("connecting to Dynamo");
+        //console.log("connecting to Dynamo");
         var r;
         
         dynamoClient.put( data1,
@@ -71,8 +99,6 @@ module.exports = {
                 if (err) {
                     console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
                 } else {
-                    console.log("Added item:", JSON.stringify(result, null, 2));
-                    
                     callback(err,result);
                 }
         });
@@ -89,6 +115,30 @@ module.exports = {
         };
         
         dynamoClient.get(params, function(err, data) {
+            if (err) {
+                console.error("Unable to read item");
+                callback(err, data);
+            } else {
+                callback(err, data);
+            }
+        });
+        
+    }
+    function readSecandory(Id,indexName,column,callback){
+        console.log("connecting to Dynamo");
+                
+        var params = {
+            TableName: TABLENAME,
+            IndexName: indexName,
+            ConsistentRead : false,
+            KeyConditionExpression: column+" = :x",
+            ExpressionAttributeValues: {
+                ":x": Id
+            },
+            ProjectionExpression: "Id, firstname"
+        };
+        console.error("Params:", JSON.stringify(params, null, 3));
+        dynamoClient.query(params, function(err, data) {
             if (err) {
                 console.error("Unable to read item");
                 callback(err, data);
--- bot/testorder.js
+++ bot/testorder.js
@@ -1,7 +1,24 @@
 var ordersModel = require('./orders.js');
-//var result = ordersModel.createOrder();
 
-var id="B1MmgWN6";
+clietId="Yasser1";
+/*
+var result = ordersModel.createOrder(clietId,function(err,data){
+                if (err) {
+                    console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("Item ....", JSON.stringify(data,null, 4));
+                }
+            });
+
+var id="";
+*/
+var data = ordersModel.CheckClientOrders(clietId,function(err,data){
+                if (err) {
+                    console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("GetItem succeeded ....", JSON.stringify(data,null, 4));
+                }
+            });
 /*
 var data = ordersModel.getItem(id,function(err,data){
                 if (err) {
@@ -10,12 +27,14 @@ var data = ordersModel.getItem(id,function(err,data){
                     console.log("GetItem succeeded ....", JSON.stringify(data,null, 4));
                 }
             });
+/*
 */
+/*
 var data = ordersModel.getOrderStatus(id,function(err,data){
                 if (err) {
                     console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
                 } else {
-                    console.log("GetItem succeeded ....", JSON.stringify(data,null, 4));
+                    console.log("Missing ....", JSON.stringify(data,null, 4));
                 }
             });
- 
+ */
