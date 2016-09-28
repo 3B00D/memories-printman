3617d823dddfad455d2cfeb7cd536775994015a6
yassermog <yassermoghrabiah@gmail.com>
2016-09-28 14:51:44 +0800

    create pdf

---
+++ pdf/publish.js
@@ -0,0 +1,27 @@
+var htmlToPdf = require('html-to-pdf');
+var fs = require('fs');
+
+html="";
+fs.readFile('1.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  
+  var imagepath = "https://d1to9yjin79kmw.cloudfront.net/themes/kids/kids1/2.jpg";
+  var data = data.replace("##img1##", imagepath);
+  html=data;
+  
+
+  htmlToPdf.convertHTMLString(html, 'book.pdf',
+    function (error, success) {
+        if (error) {
+            console.log('Oh noes! Errorz!');
+            console.log(error);
+        } else {
+            console.log('Woot! Success!');
+            console.log(success);
+        }
+    }
+    );
+});
+
